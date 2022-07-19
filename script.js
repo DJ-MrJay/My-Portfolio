151496381068936fb65da6754354997de155fca8
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-07-19 18:32:56 +0300

    Add js code

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const menuItem = document.querySelectorAll('.menu-item');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuIcon = document.querySelector('.menu-icon');
 const closeIcon = document.querySelector('.close-icon');
+const openMobileModal = document.querySelectorAll('[data-modal-target]')
+const closeMobileModal = document.querySelectorAll('[data-close-button]')
+const overlay = document.querySelector('#overlay')
 
 function toggleMenu() {
   if (menu.classList.contains('showMenu')) {
@@ -23,3 +26,36 @@ menuItem.forEach(
     menuItem.addEventListener('click', toggleMenu);
   },
 );
+
+openMobileModal.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal-container.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeMobileModal.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal-container')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
